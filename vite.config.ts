20783c47b6ded9d32380c84889523505c5a19a7c
SabianRobi <sabian.robi@gmail.com>
2025-04-06 23:25:43 +0200

    chore: Set up tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import deno from "@deno/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 import "react";
 import "react-dom";
@@ -9,7 +10,7 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react(), deno()],
+  plugins: [react(), deno(), tailwindcss()],
   optimizeDeps: {
     include: ["react/jsx-runtime"],
   },
